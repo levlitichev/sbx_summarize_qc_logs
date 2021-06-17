dbe40fcc8d8565dc95fa72c4255164c1f9fb7784
Lev Litichevskiy <levlitichev@gmail.com>
2021-06-17 15:55:19 -0400

    initial commit

---
+++ summarize_qc_logs.py
@@ -0,0 +1,204 @@
+"""
+summarize_qc_logs.py
+
+This function finds and summarizes log files made during QC steps of Sunbeam. Output is a single text file.
+
+Currently only works for paired-end data.
+
+"""
+
+import pandas as pd
+import argparse
+import os
+import sys
+import glob
+import re
+
+__author__ = "Lev Litichevskiy"
+__email__ = "levlitichev@gmail.com"
+
+CUTADAPT_COLUMN_NAMES = ["filename", "cutadapt_read_pairs_processed", "cutadapt_read_pairs_written",
+                         "cutadapt_bps_processed", "cutadapt_bps_written"]
+TRIMMOMATIC_COLUMN_NAMES = ["filename", "trimmomatic_read_pairs_processed", "trimmomatic_read_pairs_written"]
+KOMPLEXITY_COLUMN_NAMES = ["filename", "komplexity_read_pairs_removed"]
+DECONTAM_COLUMN_NAMES = ["filename", "decontam_read_pairs_written", "decontam_read_pairs_removed"]
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Required args
+    parser.add_argument("--qc_log_dir", "-i", required=True, type=str,
+                        help="path to sunbeam_output/qc/log directory")
+    parser.add_argument("--is_single_end", "-s", action="store_true", default=False,
+                        help="indicate whether data is single-end, default is paired-end")
+    parser.add_argument("--out_path", "-o", type=str, default="qc_summary.txt",
+                        help="path to output file")
+
+    return parser
+
+
+def get_cutadapt_paths(qc_log_dir):
+    cutadapt_wildcard = os.path.join(qc_log_dir, "cutadapt", "*.log")
+    cutadapt_paths = glob.glob(os.path.expanduser(cutadapt_wildcard))
+
+    assert len(cutadapt_paths) > 0, f"No cutadapt files found with the following wildcard: {cutadapt_wildcard}"
+
+    return cutadapt_paths
+
+
+def cutadapt_summary(cutadapt_paths, is_single_end=False):
+
+    list_of_lists = [""] * len(cutadapt_paths)
+
+    for ii, this_path in enumerate(cutadapt_paths):
+        this_filename = os.path.splitext(os.path.basename(this_path))[0]
+        this_file_handle = open(this_path, "r")
+
+        for line in this_file_handle:
+            if re.search("Total read pairs processed", line):
+                this_read_pairs_processed = int(line.split()[-1].replace(",", ""))
+            elif re.search("Pairs written", line):
+                this_read_pairs_written = int(line.split()[-2].replace(",", ""))
+            elif re.search("Total basepairs processed", line):
+                this_bps_processed = int(line.split()[-2].replace(",", ""))
+            elif re.search("Total written", line):
+                this_bps_written = int(line.split()[-3].replace(",", ""))
+            else:
+                pass
+        this_file_handle.close()
+
+        list_of_lists[ii] = [this_filename,
+                             this_read_pairs_processed, this_read_pairs_written,
+                             this_bps_processed, this_bps_written]
+
+    out_df = pd.DataFrame(list_of_lists, columns=CUTADAPT_COLUMN_NAMES)
+    return out_df
+
+
+def get_trimmomatic_paths(qc_log_dir):
+    trimmomatic_wildcard = os.path.join(qc_log_dir, "trimmomatic", "*.out")
+    trimmomatic_paths = glob.glob(os.path.expanduser(trimmomatic_wildcard))
+
+    assert len(trimmomatic_paths) > 0, f"No trimmomatic files found with the following wildcard: {trimmomatic_wildcard}"
+
+    return trimmomatic_paths
+
+
+def trimmomatic_summary(trimmomatic_paths, is_single_end=False):
+
+    list_of_lists = [""] * len(trimmomatic_paths)
+
+    for ii, this_path in enumerate(trimmomatic_paths):
+        this_filename = os.path.splitext(os.path.basename(this_path))[0]
+        this_file_handle = open(this_path, "r")
+
+        for line in this_file_handle:
+            if re.search("Input Read Pairs", line):
+                line_split_up = line.split()
+                this_read_pairs_processed = int(line_split_up[3])
+                this_read_pairs_written = int(line_split_up[6])
+                break
+        this_file_handle.close()
+
+        list_of_lists[ii] = [this_filename, this_read_pairs_processed, this_read_pairs_written]
+
+    out_df = pd.DataFrame(list_of_lists, columns=TRIMMOMATIC_COLUMN_NAMES)
+    return out_df
+
+
+def get_komplexity_paths(qc_log_dir):
+    komplexity_wildcard = os.path.join(qc_log_dir, "komplexity", "*.filtered_ids")
+    komplexity_paths = glob.glob(os.path.expanduser(komplexity_wildcard))
+
+    assert len(komplexity_paths) > 0, f"No komplexity files found with the following wildcard: {komplexity_wildcard}"
+
+    return komplexity_paths
+
+
+def komplexity_summary(komplexity_paths, is_single_end=False):
+
+    list_of_lists = [""] * len(komplexity_paths)
+
+    for ii, this_path in enumerate(komplexity_paths):
+        this_filename = os.path.splitext(os.path.basename(this_path))[0]
+        this_file_handle = open(this_path, "r")
+        this_read_pairs_removed = len(this_file_handle.readlines())
+        this_file_handle.close()
+
+        list_of_lists[ii] = [this_filename, this_read_pairs_removed]
+
+    out_df = pd.DataFrame(list_of_lists, columns=KOMPLEXITY_COLUMN_NAMES)
+    return out_df
+
+
+def get_decontam_paths(qc_log_dir):
+    decontam_wildcard = os.path.join(qc_log_dir, "decontam", "*.txt")
+    decontam_paths = glob.glob(os.path.expanduser(decontam_wildcard))
+
+    assert len(decontam_paths) > 0, f"No decontam files found with the following wildcard: {decontam_wildcard}"
+
+    return decontam_paths
+
+
+def decontam_summary(decontam_paths, is_single_end=False):
+
+    list_of_lists = [""] * len(decontam_paths)
+
+    for ii, this_path in enumerate(decontam_paths):
+        this_filename = os.path.splitext(os.path.basename(this_path))[0]
+
+        in_df = pd.read_table(this_path)
+
+        this_read_pairs_written = int(in_df.loc[:, "nonhost"][0])
+        this_read_pairs_removed = int(in_df.loc[:, "host"][0])
+
+        list_of_lists[ii] = [this_filename,
+                             this_read_pairs_written, this_read_pairs_removed]
+
+    out_df = pd.DataFrame(list_of_lists, columns=DECONTAM_COLUMN_NAMES)
+    return out_df
+
+
+def combine_into_one(cutadapt_df, trimmomatic_df, komplexity_df, decontam_df, is_single_end=False):
+    out_df = cutadapt_df.merge(trimmomatic_df, on="filename").merge(komplexity_df, on="filename").merge(decontam_df, on="filename")
+    return out_df
+
+
+def write_to_file(out_df, out_path):
+    out_df.to_csv(out_path, sep="\t", index=False)
+
+
+def summarize_qc_logs_main(args):
+
+    # cutadapt
+    cutadapt_paths = get_cutadapt_paths(args.qc_log_dir)
+    cutadapt_df = cutadapt_summary(cutadapt_paths, args.is_single_end)
+
+    # trimmomatic
+    trimmomatic_paths = get_trimmomatic_paths(args.qc_log_dir)
+    trimmomatic_df = trimmomatic_summary(trimmomatic_paths, args.is_single_end)
+
+    # komplexity
+    komplexity_paths = get_komplexity_paths(args.qc_log_dir)
+    komplexity_df = komplexity_summary(komplexity_paths, args.is_single_end)
+
+    # decontam
+    decontam_paths = get_decontam_paths(args.qc_log_dir)
+    decontam_df = decontam_summary(decontam_paths, args.is_single_end)
+
+    # combine all results
+    out_df = combine_into_one(cutadapt_df, trimmomatic_df, komplexity_df, decontam_df, args.is_single_end)
+
+    # write output
+    write_to_file(out_df, args.out_path)
+
+
+def main():
+    args = build_parser().parse_args(sys.argv[1:])
+    summarize_qc_logs_main(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_summarize_qc_logs.py
@@ -0,0 +1,181 @@
+import os
+import unittest
+import pandas as pd
+import summarize_qc_logs
+
+ASSETS_DIR = "test_summarize_qc_logs/"
+
+
+class TestSummarizeQcLogs(unittest.TestCase):
+
+    def test_get_cutadapt_paths(self):
+
+        expected_paths = [os.path.join(ASSETS_DIR, "cutadapt", "sample_1.log"),
+                          os.path.join(ASSETS_DIR, "cutadapt", "sample_2.log"),
+                          os.path.join(ASSETS_DIR, "cutadapt", "sample_3.log")]
+        out_paths = summarize_qc_logs.get_cutadapt_paths(ASSETS_DIR)
+        self.assertCountEqual(expected_paths, out_paths)
+
+        with self.assertRaises(AssertionError) as e:
+            summarize_qc_logs.get_cutadapt_paths("bad_path")
+        self.assertIn("No cutadapt files found", str(e.exception))
+
+    def test_cutadapt_paired(self):
+        in_paths = [os.path.join(ASSETS_DIR, "cutadapt", "sample_1.log"),
+                          os.path.join(ASSETS_DIR, "cutadapt", "sample_2.log"),
+                          os.path.join(ASSETS_DIR, "cutadapt", "sample_3.log")]
+        expected_df = pd.DataFrame([["sample_1", 4521113, 4521113, 1356333900, 1236192329],
+                                    ["sample_2", 7301623, 7301623, 2190486900, 2076703029],
+                                    ["sample_3", 456123, 555555, 200000, 180000]],
+                                   columns=["filename",
+                                            "cutadapt_read_pairs_processed",
+                                            "cutadapt_read_pairs_written",
+                                            "cutadapt_bps_processed",
+                                            "cutadapt_bps_written"])
+        out_df = summarize_qc_logs.cutadapt_summary(in_paths, is_single_end=False)
+        pd.testing.assert_frame_equal(expected_df, out_df)
+
+    def test_get_trimmomatic_paths(self):
+
+        expected_paths = [os.path.join(ASSETS_DIR, "trimmomatic", "sample_1.out"),
+                          os.path.join(ASSETS_DIR, "trimmomatic", "sample_2.out"),
+                          os.path.join(ASSETS_DIR, "trimmomatic", "sample_3.out")]
+        out_paths = summarize_qc_logs.get_trimmomatic_paths(ASSETS_DIR)
+        self.assertCountEqual(expected_paths, out_paths)
+
+        with self.assertRaises(AssertionError) as e:
+            summarize_qc_logs.get_trimmomatic_paths("bad_path")
+        self.assertIn("No trimmomatic files found", str(e.exception))
+
+
+    def test_trimmomatic_paired(self):
+        in_paths = [os.path.join(ASSETS_DIR, "trimmomatic", "sample_1.out"),
+                    os.path.join(ASSETS_DIR, "trimmomatic", "sample_2.out"),
+                    os.path.join(ASSETS_DIR, "trimmomatic", "sample_3.out")]
+        expected_df = pd.DataFrame([["sample_1", 4521113, 4309073],
+                                    ["sample_2", 7301623, 7121205],
+                                    ["sample_3", 100000, 50]],
+                                   columns=["filename",
+                                            "trimmomatic_read_pairs_processed",
+                                            "trimmomatic_read_pairs_written"])
+        out_df = summarize_qc_logs.trimmomatic_summary(in_paths, is_single_end=False)
+        pd.testing.assert_frame_equal(expected_df, out_df)
+
+
+    def test_get_komplexity_paths(self):
+
+        expected_paths = [os.path.join(ASSETS_DIR, "komplexity", "sample_1.filtered_ids"),
+                          os.path.join(ASSETS_DIR, "komplexity", "sample_2.filtered_ids"),
+                          os.path.join(ASSETS_DIR, "komplexity", "sample_3.filtered_ids")]
+        out_paths = summarize_qc_logs.get_komplexity_paths(ASSETS_DIR)
+        self.assertCountEqual(expected_paths, out_paths)
+
+        with self.assertRaises(AssertionError) as e:
+            summarize_qc_logs.get_komplexity_paths("bad_path")
+        self.assertIn("No komplexity files found", str(e.exception))
+
+
+    def test_komplexity_paired(self):
+        in_paths = [os.path.join(ASSETS_DIR, "komplexity", "sample_1.filtered_ids"),
+                    os.path.join(ASSETS_DIR, "komplexity", "sample_2.filtered_ids"),
+                    os.path.join(ASSETS_DIR, "komplexity", "sample_3.filtered_ids")]
+        expected_df = pd.DataFrame([["sample_1", 3],
+                                    ["sample_2", 20],
+                                    ["sample_3", 0]],
+                                   columns=["filename",
+                                            "komplexity_read_pairs_removed"])
+        out_df = summarize_qc_logs.komplexity_summary(in_paths, is_single_end=False)
+        pd.testing.assert_frame_equal(expected_df, out_df)
+
+    def test_get_decontam_paths(self):
+        expected_paths = [os.path.join(ASSETS_DIR, "decontam", "sample_1.txt"),
+                          os.path.join(ASSETS_DIR, "decontam", "sample_2.txt"),
+                          os.path.join(ASSETS_DIR, "decontam", "sample_3.txt")]
+        out_paths = summarize_qc_logs.get_decontam_paths(ASSETS_DIR)
+        self.assertCountEqual(expected_paths, out_paths)
+
+        with self.assertRaises(AssertionError) as e:
+            summarize_qc_logs.get_decontam_paths("bad_path")
+        self.assertIn("No decontam files found", str(e.exception))
+
+    def test_decontam_paired(self):
+        in_paths = [os.path.join(ASSETS_DIR, "decontam", "sample_1.txt"),
+                    os.path.join(ASSETS_DIR, "decontam", "sample_2.txt"),
+                    os.path.join(ASSETS_DIR, "decontam", "sample_3.txt")]
+        expected_df = pd.DataFrame([["sample_1", 3623880, 512070],
+                                    ["sample_2", 300, 200],
+                                    ["sample_3", 3000, 5]],
+                                   columns=["filename",
+                                            "decontam_read_pairs_written",
+                                            "decontam_read_pairs_removed"])
+        out_df = summarize_qc_logs.decontam_summary(in_paths, is_single_end=False)
+        pd.testing.assert_frame_equal(expected_df, out_df)
+
+    def test_combine_into_one_paired(self):
+        cutadapt_df = pd.DataFrame([["sample_1", 1, 2, 3, 4],
+                                    ["sample_2", 5, 7, 8, 6]],
+                                   columns=["filename",
+                                            "cutadapt_read_pairs_processed",
+                                            "cutadapt_read_pairs_written",
+                                            "cutadapt_bps_processed",
+                                            "cutadapt_bps_written"])
+        trimmomatic_df = pd.DataFrame([["sample_1", 8, 10],
+                                       ["sample_2", 11, 12]],
+                                   columns=["filename",
+                                            "trimmomatic_read_pairs_processed",
+                                            "trimmomatic_read_pairs_written"])
+        komplexity_df = pd.DataFrame([["sample_1", 3], ["sample_2", 20]],
+                                     columns=["filename", "komplexity_read_pairs_removed"])
+        decontam_df = pd.DataFrame([["sample_1", 20, 30],
+                                    ["sample_2", 33, 31]],
+                                   columns=["filename",
+                                            "decontam_read_pairs_written",
+                                            "decontam_read_pairs_removed"])
+
+        expected_df = pd.DataFrame([["sample_1", 1, 2, 3, 4, 8, 10, 3, 20, 30],
+                                    ["sample_2", 5, 7, 8, 6, 11, 12, 20, 33, 31]],
+                                   columns=["filename",
+                                            "cutadapt_read_pairs_processed", "cutadapt_read_pairs_written",
+                                            "cutadapt_bps_processed", "cutadapt_bps_written",
+                                            "trimmomatic_read_pairs_processed", "trimmomatic_read_pairs_written",
+                                            "komplexity_read_pairs_removed",
+                                            "decontam_read_pairs_written", "decontam_read_pairs_removed"])
+
+        out_df = summarize_qc_logs.combine_into_one(cutadapt_df, trimmomatic_df, komplexity_df, decontam_df, is_single_end=False)
+        pd.testing.assert_frame_equal(expected_df, out_df)
+
+    def test_write_to_file(self):
+        out_df = pd.DataFrame([[1, 2], [3, 4]], columns=["filename", "cutadapt_something"])
+        out_path = "test_write_qc_summary.txt"
+
+        # file should not already exist
+        self.assertTrue(not(os.path.exists(out_path)), f"{out_path} should not yet exist")
+
+        # write file
+        summarize_qc_logs.write_to_file(out_df, out_path)
+
+        # make sure it now exists
+        self.assertTrue(os.path.exists(out_path))
+
+        # clean up
+        os.remove(out_path)
+
+    def test_summarize_qc_logs_main_paired(self):
+        out_path = "test_summarize_qc_logs_main_paired.txt"
+
+        # file should not already exist
+        self.assertTrue(not (os.path.exists(out_path)), f"{out_path} should not yet exist")
+
+        args_string = f"-i {ASSETS_DIR} -o {out_path}"
+        args = summarize_qc_logs.build_parser().parse_args(args_string.split())
+        summarize_qc_logs.summarize_qc_logs_main(args)
+
+        # make sure output exists
+        self.assertTrue(os.path.exists(out_path))
+
+        # clean up
+        os.remove(out_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
